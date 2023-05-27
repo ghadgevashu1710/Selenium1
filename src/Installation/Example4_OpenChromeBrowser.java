2ba1a3ac4d0bf0e1a544e673f35f2255fecf9813
dell <dell@GhadgeVVR>
2023-05-27 15:08:00 +0530

    [Vashishtha]: new class

---
+++ src/Installation/Example4_OpenChromeBrowser.java
@@ -0,0 +1,14 @@
+package Installation;
+
+public class Example4_OpenChromeBrowser {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Vashoshtha");
+		
+		
+		System.out.println("Ghadge");
+		
+	}
+	
+}
