b0ec714367e10a5ff101dd081ab7fc615c64f678
dell <dell@GhadgeVVR>
2023-05-27 15:21:45 +0530

    [Vashishtha]:Change Spelling mistakes

--- src/Installation/Example4_OpenChromeBrowser.java
+++ src/Installation/Example4_OpenChromeBrowser.java
@@ -4,7 +4,7 @@ public class Example4_OpenChromeBrowser {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Vashoshtha");
+		System.out.println("Vashishtha");
 		
 		
 		System.out.println("Ghadge");
